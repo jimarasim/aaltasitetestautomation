5565c50eaa3259c228cfd773af89ee1cf726d654
JAMES K ARASIM <jaemzware@hotmail.com>
2019-08-29 20:44:44 -0700

    Browserstack implementation

--- src/test/java/io/aalta/Common/BaseTest.java
+++ src/test/java/io/aalta/Common/BaseTest.java
@@ -3,6 +3,8 @@ package io.aalta.Common;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import org.testng.Assert;
 import org.testng.ITestResult;
@@ -11,6 +13,8 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
+import java.net.URL;
+
 
 public class BaseTest {
     protected WebDriver driver = null;
@@ -18,7 +22,7 @@ public class BaseTest {
 
     @Parameters("browser")
     @BeforeMethod(alwaysRun = true)
-    public void BeforeTest(@Optional("CHROME") String browser) {
+    public void BeforeTest(@Optional("CHROME") String browser) throws Exception {
         //set desired browser
         try {
             this.browser = BrowserEnum.valueOf(browser);
@@ -46,8 +50,11 @@ public class BaseTest {
         }
     }
 
-    private WebDriver StartDriver() {
+    private WebDriver StartDriver() throws Exception {
         WebDriver driverToLaunch = null;
+        DesiredCapabilities cap = new DesiredCapabilities();
+        String accessKey = "qRNaU9uphj3nGGF3WzZv";
+        String userName = "jamesarasim1";
 
         if(browser == BrowserEnum.CHROME) {
             //specify the location of chromedriver (download chromedriver to working directory)
@@ -65,7 +72,27 @@ public class BaseTest {
             driverToLaunch = new FirefoxDriver();
         } else if (browser == BrowserEnum.SAFARI) {
             driverToLaunch = new SafariDriver();
+        } else if (browser.toString().contains("BROWSERSTACK")) {
+            if (browser == BrowserEnum.BROWSERSTACK_SAFARI_IPHONE){
+                cap.setCapability("browserName", "iPhone");
+                cap.setCapability("device", "iPhone 8 Plus");
+                cap.setCapability("realMobile", "true");
+                cap.setCapability("os_version", browser.version);
+                cap.setCapability("name", "Aalta Iphone Test");
+            } else {
+                cap.setBrowserName(browser.browserName);
+                cap.setPlatform(browser.platform);
+                cap.setVersion(browser.version);
+                cap.setCapability("name", "Aalta "+browser.browserName+" "+browser.platform+" TEST");
+            }
+
+            driverToLaunch = new RemoteWebDriver(new URL("http://"+userName+":"+accessKey+"@hub-cloud.browserstack.com/wd/hub"), cap);
+        } else {
+            throw new Exception("UNEXPECTED STARTDRIVER()");
         }
+
+
+
 
         return driverToLaunch;
     }
--- src/test/java/io/aalta/Common/BrowserEnum.java
+++ src/test/java/io/aalta/Common/BrowserEnum.java
@@ -1,7 +1,28 @@
 package io.aalta.Common;
 
+import org.openqa.selenium.Platform;
+
 public enum BrowserEnum {
-    FIREFOX,
-    SAFARI,
-    CHROME;
+    FIREFOX("firefox","",Platform.MAC),
+    SAFARI("safari","",Platform.MAC),
+    CHROME("chrome","",Platform.MAC),
+    BROWSERSTACK_CHROME_MAC("chrome","",Platform.MAC),
+    BROWSERSTACK_FIREFOX_MAC("firefox","",Platform.MAC),
+    BROWSERSTACK_SAFARI_MAC("safari","",Platform.MAC),
+    BROWSERSTACK_CHROME_WINDOWS("chrome","",Platform.WINDOWS),
+    BROWSERSTACK_FIREFOX_WINDOWS("firefox","",Platform.WINDOWS),
+    BROWSERSTACK_SAFARI_WINDOWS("safari","",Platform.WINDOWS),
+    BROWSERSTACK_SAFARI_IPHONE("iPhone", "11", Platform.IOS);
+
+    public final String browserName;
+    public final String version;
+    public final Platform platform;
+
+    BrowserEnum(final String browserName, final String version, final Platform platform) {
+        this.browserName = browserName;
+        this.version = version;
+        this.platform = platform;
+    }
+
+
 }
